ade0b0e14fe82f192a906dc922118799a3082a34
root <dholland@chapman.edu>
2020-04-04 05:08:36 +0000

    Finalized the part in the main method so the user can keep analyzing files if they wish to. I also got everything to compile.

--- main.cpp
+++ main.cpp
@@ -7,14 +7,27 @@ int main()
 {
   string filename; //user input
   GenStack s(100);
+  char r = 'Y';
+  string filename2;
   cout << "Hello! Please input the source code file: " << endl;
   cin >> filename;
-  if (s.check(filename) == true){
-    cout << "No errors found." << endl;
-  }
-  if (s.check(filename) == false){
-    return 0; //exits so the user can fix the error
-  }
-
+    while (s.check(filename) == true){ //loop so they can keep checking
+      cout << "No errors found." << endl;
+      cout << "Would you like to analyze another file?" << endl;
+      cout << " Type 'Y' for yes, or 'N' for no." << endl;
+      cin >> r;
+      if(r == 'Y'){
+        cout << "Please input the source code file: " << endl;
+        cin >> filename2;
+        filename = filename2;
+        s.check(filename);
+      }
+      if(r == 'N'){
+        return 0;
+      }
+    }
+    if (s.check(filename) == false){
+      return 0; //exits so the user can fix the error
+    }
 
 }
